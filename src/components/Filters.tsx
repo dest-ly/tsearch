e5554f766410a7487a00a84caed4faef99b125e2
Dest <michael.octavo@ontariotechu.net>
2026-03-19 16:21:19 -0400

    Input + Logs scaffholding

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -3,7 +3,41 @@ import styles from '../styles/Filters.module.css'
 export function Filters() {
     return (
         <div className={styles.transactions}>
-            <h3>Filters</h3>
+            <div>
+                <h3>Upper Details</h3>
+            </div>
+            <div className={styles.filters}>
+                <div>
+                    <label htmlFor="date">Date From</label>
+                    <input type="date" id="date" name="date" />
+                </div>
+                
+
+                <div>
+                    <label htmlFor="date">Date To</label>
+                    <input type="date" id="date" name="date" />
+                </div>
+
+                <div>
+                    <label htmlFor="date">ATM ID</label>
+                    <input type="number" id="date" name="date" />
+                </div>
+
+                <div>
+                    <label htmlFor="date">Customer PAN Number</label>
+                    <input type="search" id="date" name="date" />
+                </div>
+
+                <div>
+                    <label htmlFor="date">EMV chip AID</label>
+                    <input type="search" id="date" name="date" />
+                </div>
+
+                <div>
+                    <label htmlFor="date">Transaction Serial Number</label>
+                    <input type="search" id="date" name="date" />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -4,6 +4,32 @@ export function Transactions() {
     return (
         <div className={styles.transactions}>
             <h3>Transactions</h3>
+
+            <table style={{width: "100%"}}>
+                {/* Header */}
+                <tr>
+                    <th>Date</th>
+                    <th>ATM ID</th>
+                    <th>Customer PAN</th>
+                    <th>Description</th>
+                    <th>Code</th>
+                </tr>
+                {/* Contents */}
+                <tr>
+                    <td>11/05/2020</td>
+                    <td>TT1121</td>
+                    <td>430395XXXXXX5987</td>
+                    {/* Logs, use ul here */}
+                    <td>
+                        <ul>
+                            <li>Card inserted</li>
+                            <li>Card inserted</li>
+                            <li>Card inserted</li>
+                        </ul>
+                    </td>
+                    <td>11/05/20 21:35:43</td>
+                </tr>
+            </table>
         </div>
     )
 }
